3c13326738424b7296f62355a4c2687c1f7eebf2
anisakhae <anisa.khaerani12@gmail.com>
2022-07-20 21:37:57 +0700

    abcsnms

--- src/component/Rectangle.jsx
+++ src/component/Rectangle.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import './Rectangle.css'
 import Oval from '../img/Oval.svg'
+import Bitmap from '../img/Bitmap-1.png'
 
 const Rectangle = () => {
   return (
     <div className="rc-container">
         <div className="rectangle">
-            <div className="rec-text">
-                
+            <div className="rec-text"> WEEKEND FROM HOME
             </div>
             <i className="rec-text-1">stay active with a little workout</i>
-            {/* <img src={Oval} alt="" /> */}
         </div>
+        <div className="pic-1">
+          <img src={Oval} alt="" />
+        </div>
+        <div className="pic-2">
+          <img src={Bitmap} alt="" />
+        </div>
+        
     </div>
   )
 }
